28a7b9d92716aecb58efad1f8072eb4910c14b6c
Taro Morita <morita0079@gmail.com>
2026-01-16 16:57:03 +0900

    coform settings for rpst-v2

---
+++ dot_config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,22 @@
+-- conform.nvim設定
+-- php-cs-fixerで除外設定を正しく動作させるための設定
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      log_level = vim.log.levels.DEBUG,
+      formatters = {
+        php_cs_fixer = {
+          command = "php-cs-fixer",
+          args = {
+            "fix",
+            "--path-mode=intersection",
+            "$FILENAME",
+          },
+          stdin = false,
+          tmpfile_format = "$FILENAME",
+        },
+      },
+    },
+  },
+}
