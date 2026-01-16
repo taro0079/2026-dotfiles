fdbe3c9a79c76c72ef39940b40335a6ad0da7f3e
Taro Morita <morita0079@gmail.com>
2026-01-16 17:10:10 +0900

    add zsh plugin install script

---
+++ dot_local/scripts/executable_zsh-plugin-install.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
+
