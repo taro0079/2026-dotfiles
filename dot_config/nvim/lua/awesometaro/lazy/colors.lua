134c33ddf08d2daedc593357ec24b74829cf0988
Taro Morita <morita0079@gmail.com>
2026-02-05 11:16:37 +0900

    rebuild neovim configuration

---
+++ dot_config/nvim/lua/awesometaro/lazy/colors.lua
@@ -0,0 +1,59 @@
+function ColorMyPencils(color)
+	color = color or "rose-pine-moon"
+	vim.cmd.colorscheme(color)
+
+	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+return {
+	{
+		"erikbackman/brightburn.vim"
+	},
+	{
+		"ellisonleao/gruvbox.nvim",
+		name = "gruvbox",
+		config = function()
+			require("gruvbox").setup({
+				terminal_color = true,
+				undercurl = true,
+				underline = false,
+				bold = true,
+				italic = {
+					strings = false,
+					emphasis = false,
+					comments = false,
+					operators = false,
+					folds = false
+				},
+				strikethrough = true,
+				invert_selection = false,
+				invert_signs = false,
+				invert_tabline = false,
+				invert_indentguides = false,
+				inverse = true,
+				contrast = "",
+				pallette_overrides = {},
+				overrides = {},
+				dim_inactive = false,
+				transparent_mode = false
+			})
+		end
+	},
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		config = function()
+			require("rose-pine").setup({
+				disable_background = true,
+				styles = {
+					italic = false
+				},
+			})
+
+			ColorMyPencils();
+		end
+	}
+
+}
+
