58a03f96f5a3bd6c03543a7185ff0238d59e15a0
Taro Morita <morita0079@gmail.com>
2026-03-10 23:49:39 +0900

    add some settings

--- dot_local/scripts/executable_path_to_clipboard.rb
+++ dot_local/scripts/executable_path_to_clipboard.rb
@@ -3,5 +3,6 @@ require 'clipboard'
 
 file_path = ARGV[0]
 line = ARGV[1]
-Clipboard.copy("#{file_path}:#{line}")
+line_end = ARGV[2]
+Clipboard.copy("#{file_path}:#{line}-#{line_end}")
 puts 'Copied to clipboard:'
